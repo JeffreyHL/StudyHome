564c6249918c2833d8edce03524d137464373776
Jeffrey <huleiandjeffrey@sina.com>
2016-09-28 16:34:46 +0800

    thread Demo Stydy

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # StudyHome
 
-Jeffrey少爷的Java学习试验基地!
+Jeffrey少爷的Java学习试验基地！
 
-习惯养成，从GitHub开始!
+习惯养成，从GitHub开始！
+
+项目在GitHub，博客在CSDN，微信还有公众号【HL少爷家】
---
+++ src/test/thread/Soldier.java
@@ -0,0 +1,21 @@
+package test.thread;
+
+public class Soldier implements Runnable {
+
+	private String name = "";
+
+	public Soldier(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				System.out.println(name + "执行任务 " + Commander.queue.get());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
